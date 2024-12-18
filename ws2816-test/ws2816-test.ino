0d9c8d39415a92be95b4bee71c286d20dd780679
jasoncoon <3598755+jasoncoon@users.noreply.github.com>
2024-12-18 16:51:25 -0600

    Simplify

--- ws2816-test/ws2816-test.ino
+++ ws2816-test/ws2816-test.ino
@@ -8,48 +8,37 @@ FASTLED_USING_NAMESPACE
 #define NUM_LEDS 64
 #define NUM_LEDS2 NUM_LEDS * 2
 
-#define MILLI_AMPS 1600 // IMPORTANT: set the max milli-Amps of your power supply (4A = 4000mA)
-#define FRAMES_PER_SECOND 240
-
 CRGB leds[NUM_LEDS];
 
 CRGB leds2[NUM_LEDS2];
-
-uint8_t brightness = 128;
 
 void setup()
 {
   Serial.begin(115200);
 
-  // Serial.println("serial delay...");
-  // delay(3000);
-
+  // FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds2, NUM_LEDS2);
-  FastLED.setDither(false);
-  FastLED.setCorrection(0);
-  FastLED.setTemperature(0);
-  FastLED.setBrightness(brightness);
-  FastLED.setMaxPowerInVoltsAndMilliamps(5, MILLI_AMPS);
-  // fill_solid(leds2, NUM_LEDS2, CRGB::Black);
-  // FastLED.show();
-
-  FastLED.setBrightness(brightness);
+  // FastLED.setDither(0);
+  // FastLED.setCorrection(0);
+  // FastLED.setTemperature(0);
 }
 
 void loop()
 {
-  fill_solid(leds, NUM_LEDS, CRGB::Red);
+  fill_solid(leds2, NUM_LEDS2, CRGB::Red);
 
-  for (uint16_t i = 0; i < NUM_LEDS; i++)
-  {
-    CRGB led = leds[i];
-    uint8_t r = led.r;
-    uint8_t g = led.g;
-    uint8_t b = led.b;
+  // fill_solid(leds, NUM_LEDS, CRGB::Red);
 
-    leds2[i * 2] = CRGB(r, r, g);
-    leds2[i * 2 + 1] = CRGB(g, b, b);
-  }
+  // for (uint16_t i = 0; i < NUM_LEDS; i++)
+  // {
+  //   CRGB led = leds[i];
+  //   uint8_t r = led.r;
+  //   uint8_t g = led.g;
+  //   uint8_t b = led.b;
+
+  //   leds2[i * 2] = CRGB(r, r, g);
+  //   leds2[i * 2 + 1] = CRGB(g, b, b);
+  // }
 
   FastLED.show();
 }
